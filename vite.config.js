c87c9485bbd567a6ba7a7db4a3440924d81f6d36
waed <waedhamdosheikh@gamil.com>
2026-09-23 14:26:37 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/NewsMag/", // 👈 أضيفي هذا السطر بدقة (مع السلايدج في البداية والنهاية)
+});
